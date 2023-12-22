8f5aef6c262c47ce564976ae6d0e43387ad3e2ac
Fernando Antunes <f.antunes84@gmail.com>
2023-12-22 20:06:20 -0300

    🐛 Correção no problema das migrações

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# python manage.py makemigrations statement
-# python manage.py migrate statement
-# python manage.py makemigrations login
-# python manage.py migrate login
-# python manage.py collectstatic --no-input
+python manage.py makemigrations statement
+python manage.py migrate statement 0001_initial --fake
+python manage.py migrate statement 0002_version --fake
+python manage.py migrate statement
+python manage.py makemigrations login
+python manage.py migrate login
+python manage.py collectstatic --no-input
 
 gunicorn myfinance.wsgi:application --bind 0.0.0.0:8000
