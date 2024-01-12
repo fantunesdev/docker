554a0dcef393b6484dbb234c941095bf88db2bd7
Fernando Antunes <f.antunes84@gmail.com>
2024-01-12 19:24:54 -0300

    🔨 Update do atualizador automático

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 python manage.py makemigrations statement
 python manage.py migrate statement 0001_initial --fake
 python manage.py migrate statement 0002_version --fake
+python manage.py migrate statement 0003_alter_version_options --fake
+python manage.py migrate statement 0004_account_file_handler_conf_card_file_handler_conf --fake
 python manage.py migrate statement
 python manage.py makemigrations login
 python manage.py migrate login
