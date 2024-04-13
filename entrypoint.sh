e437310af383b96276b80d314a0d3101a4dbd9d2
Fernando Antunes <f.antunes84@gmail.com>
2024-04-13 15:48:37 -0300

    🔨 Atualizaçao do entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ python manage.py migrate statement 0001_initial --fake
 python manage.py migrate statement 0002_version --fake
 python manage.py migrate statement 0003_alter_version_options --fake
 python manage.py migrate statement 0004_account_file_handler_conf_card_file_handler_conf --fake
+python manage.py migrate statement 0005_fixedexpenses --fake
+python manage.py migrate statement 0006_dream_alter_version_options_version_date_portion --fake
 python manage.py migrate statement
 python manage.py makemigrations login
 python manage.py migrate login
